b6ef2687291250f2a4fdad1b55c2dce0635a07fc
Prajwal-B05 <prajwal.is20@bmsce.ac.in>
2023-05-05 14:54:26 +0530

    Game Test 4 passed

--- test/game4Test.js
+++ test/game4Test.js
@@ -5,15 +5,16 @@ describe('Game4', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
-
-    return { game };
+    const signer1 = ethers.provider.getSigner(0);
+    return { game , signer1};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-
+    const { game,signer1 } = await loadFixture(deployContractAndSetVariables);
+    const address1 = await signer1.getAddress();
+    await game.write(address1);
     // nested mappings are rough :}
-
-    await game.win();
+   
+    await game.win(address1);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
